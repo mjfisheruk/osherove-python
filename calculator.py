7c55e3939a1f806693251396317746362dbff0c7
Matt Fisher <general@mjfisher.net>
2020-01-19 11:32:44 +0000

    Step 3 - allow newline as delimiter as well as comma

--- calculator.py
+++ calculator.py
@@ -1,4 +1,9 @@
+def _split_into_numbers(numbers_str):
+    normalized_numbers_str = numbers_str.replace("\n", ",")
+    parts = normalized_numbers_str.split(",")
+    return [int(n) for n in parts if n.isdigit()]
+
+
 def add(numbers_str):
-    parts = numbers_str.split(",")
-    numbers = [int(n) for n in parts if n.isdigit()]
+    numbers = _split_into_numbers(numbers_str)
     return sum(numbers)
--- calculator_test.py
+++ calculator_test.py
@@ -15,6 +15,9 @@ class AddTest(unittest.TestCase):
     def test_returns_sum_of_many_comma_delimited_numbers(self):
         self.assertEqual(13, calculator.add("2,3,1,7"))
 
+    def test_allows_comma_and_newline_as_delimiters(self):
+        self.assertEqual(6, calculator.add("1\n2,3"))
+
 
 if __name__ == '__main__':
     unittest.main()
