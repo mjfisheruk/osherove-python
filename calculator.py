3bff939eefb980be3ec0c53b92d77dc3930544d2
Matt Fisher <general@mjfisher.net>
2020-01-19 11:52:14 +0000

    Step 4 - allow custom delimiter to be set

--- calculator.py
+++ calculator.py
@@ -1,9 +1,20 @@
-def _split_into_numbers(numbers_str):
-    normalized_numbers_str = numbers_str.replace("\n", ",")
-    parts = normalized_numbers_str.split(",")
+import re
+
+
+def _extract_delimiter(numbers_str):
+    m = re.match("^//(.)\n", numbers_str)
+    if m:
+        return m.group(1)
+    return ","
+
+
+def _split_into_numbers(numbers_str, delimiter):
+    normalized_numbers_str = numbers_str.replace("\n", delimiter)
+    parts = normalized_numbers_str.split(delimiter)
     return [int(n) for n in parts if n.isdigit()]
 
 
 def add(numbers_str):
-    numbers = _split_into_numbers(numbers_str)
+    delimiter = _extract_delimiter(numbers_str)
+    numbers = _split_into_numbers(numbers_str, delimiter)
     return sum(numbers)
--- calculator_test.py
+++ calculator_test.py
@@ -15,8 +15,17 @@ class AddTest(unittest.TestCase):
     def test_returns_sum_of_many_comma_delimited_numbers(self):
         self.assertEqual(13, calculator.add("2,3,1,7"))
 
+    def test_numbers_greater_than_nine_add_correctly(self):
+        self.assertEqual(18, calculator.add("1,2,12,3"))
+
     def test_allows_comma_and_newline_as_delimiters(self):
         self.assertEqual(6, calculator.add("1\n2,3"))
+
+    def test_allow_custom_delimiter(self):
+        self.assertEqual(3, calculator.add("//;\n1;2"))
+
+    def test_custom_delimiter_as_digit_is_allowed(self):
+        self.assertEqual(6, calculator.add("//1\n313"))
 
 
 if __name__ == '__main__':
