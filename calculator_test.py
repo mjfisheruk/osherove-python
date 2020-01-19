9de6f29e7f228092d4b1cd62daac5fcb448cf64a
Matt Fisher <general@mjfisher.net>
2020-01-19 11:26:37 +0000

    Step 2 - check support for aribtrary length list of numbers

--- calculator_test.py
+++ calculator_test.py
@@ -4,13 +4,16 @@ import calculator
 
 class AddTest(unittest.TestCase):
     def test_returns_zero_with_empty_string(self):
-        self.assertEqual(calculator.add(""), 0)
+        self.assertEqual(0, calculator.add(""))
 
     def test_returns_single_number_unmodified(self):
-        self.assertEqual(calculator.add("2"), 2)
+        self.assertEqual(2, calculator.add("2"))
 
-    def test_returns_sum_of_comma_delimited_numbers(self):
-        self.assertEqual(calculator.add("2,3"), 5)
+    def test_returns_sum_of_two_comma_delimited_numbers(self):
+        self.assertEqual(5, calculator.add("2,3"))
+
+    def test_returns_sum_of_many_comma_delimited_numbers(self):
+        self.assertEqual(13, calculator.add("2,3,1,7"))
 
 
 if __name__ == '__main__':
